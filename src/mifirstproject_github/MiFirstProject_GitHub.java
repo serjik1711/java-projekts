95a17d58315f47852ad787926cc486d895e21abd
serjik1711 <92232637+serjik1711@users.noreply.github.com>
2026-03-04 23:33:15 +0300

    Add files via upload

---
+++ src/mifirstproject_github/MiFirstProject_GitHub.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mifirstproject_github;
+
+/**
+ *
+ * @author User
+ */
+public class MiFirstProject_GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        
+        System.out.println("Hello word");
+    }
+    
+}
