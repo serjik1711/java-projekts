f07d13c700d43f3842ecba87531189e66cee6078
serjik1711 <92232637+serjik1711@users.noreply.github.com>
2026-04-16 00:13:05 +0300

    Add files via upload

--- MyFirstProject/src/myfirstproject/MyFirstProject.java
+++ MyFirstProject/src/myfirstproject/MyFirstProject.java
@@ -8,13 +8,12 @@ package myfirstproject;
 //import java.math.BigDecimal;
 
 import java.util.Arrays;
-
+//import java.lang.Math;
 //import java.math.MathContext;
-//import java.math.RoundingMode;
+import java.math.RoundingMode;
 //import java.util.Arrays;
-
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 /**
  *
  * @author Сергей
@@ -25,28 +24,31 @@ public class MyFirstProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        System.out.println(feast("adfd","afab"));
-    } 
-    public static boolean feast(String beast, String dish) {
-       
-       boolean start1=beast.startsWith("a");
-       boolean finish1=beast.endsWith("b");
-       boolean start2=dish.startsWith("a");
-       boolean finish2=dish.endsWith("b");
-       boolean rezF1=false;
-       boolean rezF2=false;
-       
-       if(start1==start2){rezF1=true;}
-          if(finish1==finish2){rezF2=true;}
-             if(rezF1==rezF2){return true;}
-
-    return false;
     
-  }
+        System.out.println(finalGrade(52, 1));
+                
+    }
+ 
+  
+    public static int finalGrade(int exam, int projects){
+       int result=0,result1=0,resOk=0;
+       
+      if(exam<50){result=0;}
+        if(exam>50&&exam<76){result=75;}
+          if(exam>75&&exam<91){result=90;}
+             if(exam>90){result=100;}
+              if(projects<2){result1=0;}
+                 if(projects>1&&projects<5){result1=75;}
+                    if(projects>4&&projects<11){result1=90;}
+                       if(projects>10){result1=100;}
+                         if(result1>0){resOk=result1;}else resOk=result;
+             
+            return resOk;
+    }
+       
     
-}
    
+} 
 
 
 
